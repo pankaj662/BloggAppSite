8ef5e405a8ddbf234c7efd2fa3f95728f2878779
Pankaj <pankajdhakad585@gmail.com>
2025-12-18 10:14:29 +0530

    Time:-10:14

--- src/main/java/com/gray/Entity/IpTracking.java
+++ src/main/java/com/gray/Entity/IpTracking.java
@@ -8,12 +8,18 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "ip")
+@Table(
+	    name = "ip",
+	    uniqueConstraints  = {
+	        @UniqueConstraint(columnNames = {"user_id", "ip_address"})
+	    }
+	  )  
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
