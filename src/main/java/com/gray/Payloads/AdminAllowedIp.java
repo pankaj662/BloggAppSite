e18dcaff996d77a74196130288a253a4338e06c0
Pankaj <pankajdhakad585@gmail.com>
2025-12-18 10:28:05 +0530

    Time:-10:28

--- src/main/java/com/gray/Payloads/AdminAllowedIp.java
+++ src/main/java/com/gray/Payloads/AdminAllowedIp.java
@@ -10,15 +10,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class AdminAllowedIp {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
 	private int id;
 	
 	@Column(nullable = false,unique = true,updatable = false)
